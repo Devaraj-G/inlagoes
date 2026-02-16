5ff35134f53dad8929c5104433ee5e2b06e53cc0
Devaraj-G <d.gopinathan@ucl.ac.uk>
2026-02-16 14:13:40 +0000

    WIP: inla fitting

--- R/fit_inla.R
+++ R/fit_inla.R
@@ -6,6 +6,7 @@ source("./R/convert_units.R")
 source("./R/load_data.R")
 source("./R/plot_data.R")
 
+# Load data
 filename = './data/noaa-goes16/ABI-L2-SSTF/2023/297/12/OR_ABI-L2-SSTF-M6_G16_s20232971200209_e20232971259517_c20232971304395.nc'
 limits = c(-110,-90,5,20)
 
@@ -13,20 +14,30 @@ data=load_data(filename,'DQF')
 plot_data(data)
 data_cropped = crop_data(data,limits)
 plot_data(data_cropped)
+dqf = data_cropped
 
 data=load_data(filename,'SST')
-plot_data(data-273)
+data=data-273
+plot_data(data)
 data_cropped = crop_data(data,limits)
 plot_data(data_cropped)
+data_good = data_cropped*(dqf == 0)
+plot_data(data_good)
+data_bad = data_cropped*(dqf != 0)
+plot_data(data_bad)
+data_cropped = data_good
+data_good = data_cropped
+data_good[dqf != 0] = NA
+plot_data(data_good)
 
 # Pick some samples from SST data
-xy_obs <- spatSample(data_cropped, 1000, method = 'random', as.points = TRUE)
+xy_obs <- spatSample(data_good, 10000, method = 'random', as.points = TRUE)
 xy_obs <- xy_obs[!is.na(values(xy_obs)),]
 
 xy_obs1 <- extract(data_cropped, xy_obs)[,2]
+xy_obs2 <- extract(data_good, xy_obs)[,2] # same as points are not on NA points
 
-
-plot_data(data_cropped)
+plot_data(data_good)
 points(xy_obs, pch = 20, col = 'red')
 
 # Prepare inputs for INLA
@@ -38,7 +49,7 @@ sst=xy_obs1
 # Create mesh
 mesh <- fm_mesh_2d_inla(
   loc = xy,
-  max.edge = c(10,1.0),
+  max.edge = c(2,0.75),
   cutoff = 0.1
 )
 plot(mesh); points(xy, pch = 19, col = 'red')
@@ -54,7 +65,7 @@ fit_stack <- inla.stack(
   effects = list(spatial = 1:spde$n.spde, data.frame(intercept = 1))
 )
 
-formula <- y ~0 + intercept + f(spatial, model = spde)
+formula <- y ~0 + f(spatial, model = spde)
 
 # fit INLA
 
@@ -63,8 +74,61 @@ fit_inla <- inla(
   data = inla.stack.data(fit_stack),
   family = "gaussian",
   control.predictor = list(A = inla.stack.A(fit_stack), compute = TRUE),
-  control.compute = list(config = TRUE)
+  control.compute=list(config=TRUE)
 )
+
+bru_components <- ~0 + f(spatial, model = spde)
+bru_formula <- count ~ .
+
+fit_bru <- bru(
+bru_components,
+bru_obs(
+  formula = bru_formula,
+family = 'gaussian',
+data = xy_obs1,
+),
+options = list(
+  control.compute = list(waic = TRUE, cpo = FALSE),
+  control.inla = list(
+    int.strategy = "eb"
+  )
+)
+)
+
+
+fit_inla$summary.fixed
+fit_inla$summary.hyperpar
+
+proj = inla.mesh.projector(mesh, xlim=c(-113,-89), ylim=c(4.9, 20.5), dim=c(100,100))
+msurface = inla.mesh.project(proj, fit_inla$summary.random$spatial[,"mean"])
+ssurface = inla.mesh.project(proj, fit_inla$summary.random$spatial[,"sd"])
+
+out.p = c();
+out.p$x = seq(-109, -90, length.out=100)
+out.p$y = seq(5.5, 20, length.out=100)
+out.p$z = msurface
+out.s <- out.p
+out.s$z = ssurface
+
+#x <- as.matrix(cbind(out.p$x,out.p$y))
+#z <- as.matrix(out.p$z)
+ggmatplot()
+
+ggplot() +
+  gg(msurface) +
+  scale_fill_viridis_c()
+  geom_point(aes(mesh$loc[,1],mesh$loc[,2]))
+ggplot() +
+  gg(ssurface) +
+  scale_fill_viridis_c()
+
+
+sst_pred_mean <- fit_inla$summary.fitted.values$mean
+sst_pred <- rast(xy_obs)
+values(sst_pred) <- sst_pred_mean
+
+plot(sst_pred, main = "predicted")
+points(xy, pch=19, col="black")
 
 # create grid for prediction
 xy_pr <- spatSample(data_cropped, 10000, method = 'random', as.points = TRUE)
@@ -79,7 +143,7 @@ stack_pred <- inla.stack(
                  data.frame(intercept = 1))
 )
 
-# predict
+# predict (most probably redundant, info already in fit_inla)
 pred_inla <- inla(
   formula,
   data = inla.stack.data(stack_pred),
